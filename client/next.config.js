268210707119c68a1228ebbeae9f84156a254a10
Glen <glenrcoffey@gmail.com>
2021-07-17 09:39:49 +1000

    Getting full stack working together locally at least

---
+++ client/next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  reactStrictMode: true,
+  webpackDevMiddleware: config => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    }
+
+    return config
+  },
+}
